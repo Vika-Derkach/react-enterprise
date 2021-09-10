66b5c7350b40c32d880b8226e6cd7c8eeddb810b
Vika <queenviktoriyphp@gmail.com>
2021-09-10 12:19:15 +0300

    add react package to mono repository

---
+++ packages/react/src/atoms/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+interface ButtonProps {
+  label: string;
+}
+
+const Button: React.FC<ButtonProps> = ({ label }) => {
+  return <div>{label}</div>;
+};
+export default Button;
